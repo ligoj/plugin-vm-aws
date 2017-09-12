6eb2d2f62d213fca87199bcb42726163d6d86a5f
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-09-12 17:06:35 +0200

    Fix i18n typo

--- src/main/resources/META-INF/resources/webjars/service/vm/aws/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/vm/aws/nls/fr/messages.js
@@ -1,13 +1,13 @@
 define({
-	"service:vm:aws:user" : "Utilisateur",
-	"service:vm:aws:password" : "Mot de passe",
-	"service:vm:aws:account": 'Compte',
-	"service:vm:aws:instance-id" : "Identifiant instance",
-	"service:vm:aws:resources" : "Ressources",
-	"error" : {
-		"aws-instance-i" : "Instance invlide",
-		"aws-login" : "Authentication échouée",
-		"aws-admin" : "Accès administraton échoué"
+	'service:vm:aws:user' : 'Utilisateur',
+	'service:vm:aws:password' : 'Mot de passe',
+	'service:vm:aws:account': 'Compte',
+	'service:vm:aws:id' : 'Identifiant d\'instance',
+	'service:vm:aws:resources' : 'Ressources',
+	'error' : {
+		'aws-instance-id' : 'Instance invalide',
+		'aws-login' : 'Authentication échouée',
+		'aws-admin' : 'Accès administraton échoué'
 	}
 
 });
